925ee2f800980bcd9d55e73dbf4d5b8371669c30
Nena Furtmeier <Nena@Furtmeier.IT>
2018-03-24 15:30:04 +0100

    added new db field

--- supportBox/SBInfo/server/update.php
+++ supportBox/SBInfo/server/update.php
@@ -1,3 +1,10 @@
 <?php
+require_once __DIR__."/../../../libraries/PhpFileDB.class.php";
+require_once __DIR__."/util.php";
+require_once __DIR__."/OnAction.php";
 
-echo "Nothing to do!";
+$C = util::dbConnection();
+
+$C->query("ALTER TABLE `SBForward` ADD `SBForwardURLAppend` VARCHAR(200) NOT NULL AFTER `SBForwardPort`");
+
+echo "Done!";
