9050ed60d50b303dd6aeafb75b1e05c87d523603
Nena Furtmeier <Nena@Furtmeier.IT>
2018-03-29 12:49:44 +0200

    login screen

---
+++ specifics/EnvironmentCurrent.class.php
@@ -0,0 +1,6 @@
+<?php
+class EnvironmentCurrent extends Environment {
+	public $contentLoginTop = "<div style=\"padding:10px;border-bottom:1px solid #CCC;\"><image src=\"./specifics/logo.svg\" style=\"width:70px;float:left;margin-right:20px;\">
+		<h1 class=\"prettyTitle\" style=\"padding:0;margin:0;\">supportBox</h1><p><a href=\"./ubiquitous/CustomerPage/?D=supportBox/SBControl\">Status anzeigen und ändern</a></p>
+		<div style=\"clear:both;\"></div></div>";
+}
