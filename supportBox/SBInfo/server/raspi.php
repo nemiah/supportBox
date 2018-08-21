7a381dbae200c23a4304eebc3202047767e0aa31
Nena Furtmeier <Nena@Furtmeier.IT>
2018-08-21 16:34:19 +0200

    bug fix

--- supportBox/SBInfo/server/raspi.php
+++ supportBox/SBInfo/server/raspi.php
@@ -111,8 +111,8 @@ $connection->on('open', function (\Thruway\ClientSession $session) use ($connect
 	});
 	
 	$session->register('it.furtmeier.supportbox.'.$serial.".getConnections", function(){
-		SBUtil::log(print_r(OnAction::getConnections(), true));
-		return OnAction::getConnections();
+		return SBUtil::ok("", OnAction::getInfo());
+		#return OnAction::getConnections();
 	});
 	
 	$session->register('it.furtmeier.supportbox.'.$serial.".doUpdate", function(){
