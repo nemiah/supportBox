2cabe857c249c7b64acbadc0164934192c95a4ae
Nena Furtmeier <Nena@Furtmeier.IT>
2018-10-16 15:21:06 +0200

    added interval option

---
+++ supportBox/SBInfo/server/updates/20181016.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+echo '127.0.0.1	localhost
+::1		localhost ip6-localhost ip6-loopback
+ff02::1		ip6-allnodes
+ff02::2		ip6-allrouters
+
+127.0.1.1	supportbox' | tee /etc/apache2/sites-enabled/000-default.conf > /dev/null
